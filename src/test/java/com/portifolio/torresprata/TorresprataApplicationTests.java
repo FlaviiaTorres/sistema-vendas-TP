b5fccd654301c79fc1aa1113411cd24111306be1
Flávia Torres <you@example.com>
2024-01-09 22:13:08 -0300

    Criando o projeto

---
+++ src/test/java/com/portifolio/torresprata/TorresprataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.torresprata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorresprataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
